b4ee7a74be87d3635f473bf0955b6a3549a79363
Lolippie <charlot.noemie72@gmail.com>
2026-01-15 04:18:10 +0100

    fix : route et reponses

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,26 +11,19 @@ import { Role } from 'generated/prisma';
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
  
+  @Admin()
   @Get('')
   @ApiResponse({ status: 200, description: 'List of users' }) // correspond a la doc de swagger
   async getUsers() {
     return this.usersService.getUsers();
   }
 
+  @Admin()
   @Get('ids')
   @ApiResponse({ status: 200, description: 'List of users' }) // correspond a la doc de swagger
   async getUsersByIds(@Body("ids") ids: string[]) {
     return this.usersService.getUsersByIds(ids);
   }
-  // Permet de recuperer tous les users version recuperation d'une promesse
-  // async getUsers(): Promise<any> {
-  //   const prisma = new PrismaClient(); // prisma permet d'interagir avec la base de données, il convertit
-  //   // les appels en requetes SQL
-  //   await prisma.$connect(); //declanche la connexion a la base de données
-  //   const users = await this.usersService.getUsers();    
-  //   await prisma.$disconnect(); // ferme la connexion a la base de données
-  //   return users;
-  // }
 
   @Admin()
   @HttpCode(HttpStatus.OK)
@@ -40,14 +33,14 @@ export class UsersController {
   }
 
   @Admin()
-  @HttpCode(HttpStatus.NO_CONTENT)
+  @HttpCode(HttpStatus.OK)
   @Patch('/:id')
   async updateUser(@Body() updatedUserDto: UpdateUserDto, @Param('id') id: string) {
     return this.usersService.updateUser(updatedUserDto, id);
   }
 
   @Admin()
-  @HttpCode(HttpStatus.NO_CONTENT)
+  @HttpCode(HttpStatus.OK)
   @Delete('/:id')
   async deleteUser(@Param('id') id: string) {
     return this.usersService.deleteUser(id);
